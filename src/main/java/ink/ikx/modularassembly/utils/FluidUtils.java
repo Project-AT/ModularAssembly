281bc6b655fb44ff205572587702d1ae9fdacc3c
ikexing <ikexing@qq.com>
2021-12-08 18:24:28 +0800

    remove unused methods

--- src/main/java/ink/ikx/modularassembly/utils/FluidUtils.java
+++ src/main/java/ink/ikx/modularassembly/utils/FluidUtils.java
@@ -3,7 +3,6 @@ package ink.ikx.modularassembly.utils;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.FluidUtil;
-import net.minecraftforge.fluids.capability.IFluidHandlerItem;
 import net.minecraftforge.fluids.capability.IFluidTankProperties;
 
 import java.util.Arrays;
@@ -32,13 +31,6 @@ public class FluidUtils {
         FluidStack fluidB = getFluidFromHandler(stackB);
         if (MiscUtils.areNull(fluidA, fluidB)) return false;
         return fluidA.containsFluid(fluidB);
-    }
-
-    public static void drainStackToHandler(ItemStack stack) {
-        if (stack.isEmpty() || FluidUtil.getFluidHandler(stack) == null)
-            return;
-        IFluidHandlerItem fluidHandler = FluidUtil.getFluidHandler(stack);
-        fluidHandler.drain(1000, true);
     }
 
 }

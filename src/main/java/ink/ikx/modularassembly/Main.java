76a9499c467c86e70278d9dd4dfff3287813215c
ikexing <ikexing@qq.com>
2022-01-24 14:59:07 +0800

    add crt dependency

--- src/main/java/ink/ikx/modularassembly/Main.java
+++ src/main/java/ink/ikx/modularassembly/Main.java
@@ -1,6 +1,5 @@
 package ink.ikx.modularassembly;
 
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -16,7 +15,10 @@ public class Main {
     public static final String MOD_ID = "modularassembly";
     public static final String MOD_NAME = "ModularAssembly";
     public static final String VERSION = "1.0.2";
-    public static final String DEPENDENCIES = "required-after:forge@[14.23.5.2847,);required-after:modularmachinery@[1.11.0,);after:modularcontroller@[1.2.1,)";
+    public static final String DEPENDENCIES = "required-after:forge@[14.23.5.2847,);" +
+            "required-after:crafttweaker;" +
+            "required-after:modularmachinery@[1.11.0,);" +
+            "after:modularcontroller@[1.2.1,)";
 
     @Mod.Instance
     public static Main instance;
